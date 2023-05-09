b145b1ceccb2703e2768169f7ea24a1bbb523212
Bruno Ricardo <brunoricardore@gmail.com>
2023-05-09 01:06:18 -0300

    deploy test

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/phone-simulator'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/phone-simulator/index.html'));});
+app.listen(process.env.PORT || 8080);
